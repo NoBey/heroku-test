027facaffe53a625111cdeb021d22100651d85c3
NoBey <nobey@nobey.cn>
2018-10-13 13:53:43 +0800

    test

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const koa = require('koa')
 const App = new koa()
-
+const PORT = process.env.PORT || 5000
 App.use(async(ctx) =>{
-    ctx.body = 'nobey'
+    ctx.body = 'nobey' + PORT
 })
 
-App.listen(5000)
+App.listen(PORT)
